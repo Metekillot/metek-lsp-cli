5262c95144bfc2b56ae94778f0af844512d37b62
Joshua 'Joan Metek(illot)' Kidder <anticitizenthree@gmail.com>
2026-05-08 12:38:04 -0400

    mucking about with serialization pt 3 the reckoning

--- Driver/DriverObjectTree.cs
+++ Driver/DriverObjectTree.cs
@@ -1,16 +1,16 @@
 /*
-* Copyright (c) 2026, Joshua 'Joan Metek' Kidder
-*
-* This file is part of metek-lsp-cli,
-*
-* metek-lsp-cli is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
-* as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-*
-* metek-lsp-cli is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-* 
-* You should have received a copy of the GNU Lesser General Public License along with metek-lsp-cli. If not, see <https://www.gnu.org/licenses/>. 
-*/
+ * Copyright (c) 2026, Joshua 'Joan Metek' Kidder
+ *
+ * This file is part of metek-lsp-cli,
+ *
+ * metek-lsp-cli is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * metek-lsp-cli is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with metek-lsp-cli. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 using System.Text.Json.Serialization;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
@@ -22,6 +22,7 @@ using JsonSerializer = System.Text.Json.JsonSerializer;
 
 
 namespace Metek.LspCli;
+
 [Parallel]
 [Method("experimental/dreammaker/objectTree2", Direction.ClientToServer)]
 [
@@ -32,6 +33,7 @@ public class QueryObjectTreeParams : IRequest<ObjectTreeType>
 {
     public string path;
 }
+
 [JsonSourceGenerationOptions(WriteIndented = true)]
 [JsonSerializable(typeof(ObjectTreeType))]
 [JsonSerializable(typeof(ObjectTreeVar))]
@@ -39,66 +41,60 @@ public class QueryObjectTreeParams : IRequest<ObjectTreeType>
 internal partial class SourceGenerationContext : JsonSerializerContext
 {
 }
-public record ObjectTreeType
+
+public record ObjectTreeType(
+    string name,
+    SymbolKind kind,
+    Location? location,
+    ObjectTreeVar[] vars,
+    ObjectTreeProc[] procs,
+    ObjectTreeType[] children,
+    long n_vars,
+    long n_procs,
+    long n_children
+)
 {
-    [System.Text.Json.Serialization.JsonRequired]
-    public string name;
-
-    [System.Text.Json.Serialization.JsonRequired]
-    public SymbolKind kind;
-
-    [System.Text.Json.Serialization.JsonRequired]
-    public Location? location;
-
-    [System.Text.Json.Serialization.JsonRequired]
-    public ObjectTreeVar[] vars;
-
-    [System.Text.Json.Serialization.JsonRequired]
-    public ObjectTreeProc[] procs;
-
-    [System.Text.Json.Serialization.JsonRequired]
-    public ObjectTreeType[] children;
-
-    [System.Text.Json.Serialization.JsonRequired]
-    public long n_vars;
-
-    [System.Text.Json.Serialization.JsonRequired]
-    public long n_procs;
-
-    [System.Text.Json.Serialization.JsonRequired]
-    public long n_children;
-
     public override string ToString()
-    {
-        return JsonSerializer.Serialize(this, SourceGenerationContext.Default.ObjectTreeType);
-    }
+{
+    return JsonSerializer.Serialize(this, SourceGenerationContext.Default.ObjectTreeType);
+}
+
 }
 
 public record ObjectTreeVar
 {
     [System.Text.Json.Serialization.JsonRequired]
     public string name;
+
     [System.Text.Json.Serialization.JsonRequired]
     public SymbolKind kind;
+
     [System.Text.Json.Serialization.JsonRequired]
     public Location? location;
+
     [System.Text.Json.Serialization.JsonRequired]
     public bool is_declaration;
+
     public override string ToString()
     {
         return JsonSerializer.Serialize(this, SourceGenerationContext.Default.ObjectTreeVar);
     }
 }
+
 public record ObjectTreeProc
 {
     [System.Text.Json.Serialization.JsonRequired]
     public string name;
+
     [System.Text.Json.Serialization.JsonRequired]
     public SymbolKind kind;
+
     [System.Text.Json.Serialization.JsonRequired]
     public Location? location;
+
     [System.Text.Json.Serialization.JsonRequired]
     public bool? is_verb;
+
     public override string ToString()
     {
         return JsonSerializer.Serialize(this, SourceGenerationContext.Default.ObjectTreeProc);
